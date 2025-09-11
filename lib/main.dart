eaca255a8a99a22559dbf122a68cf554f2e74974
assassinyousuf <itsmemehrab369@gmail.com>
2025-09-11 23:23:53 +0600

    Initial commit of Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,67 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Material App',
       home: Scaffold(
-        body: Center(
-          child: SizedBox(
-            width: 208, // 100 + 100 + 4*2 for margins
-            height: 208,
+          body: SingleChildScrollView(
             child: Column(
               children: [
-                Row(children: [colorBox(Colors.red), colorBox(Colors.blue)]),
-                Row(
-                  children: [colorBox(Colors.purple), colorBox(Colors.orange)],
+                Container(
+                  color: Colors.red,
+                  height: 100.0,
+                  width: double.infinity,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 100.0,
+                  width: double.infinity,
+                ),
+                Container(
+                  color: Colors.red,
+                  height: 100.0,
+                  width: double.infinity,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 100.0,
+                  width: double.infinity,
+                ),Container(
+                  color: Colors.red,
+                  height: 100.0,
+                  width: double.infinity,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 100.0,
+                  width: double.infinity,
+                ),Container(
+                  color: Colors.red,
+                  height: 100.0,
+                  width: double.infinity,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 100.0,
+                  width: double.infinity,
+                ),Container(
+                  color: Colors.red,
+                  height: 100.0,
+                  width: double.infinity,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 100.0,
+                  width: double.infinity,
                 ),
               ],
             ),
-          ),
-        ),
+          )
       ),
-    );
-  }
-
-  // Helper widget to create a fixed-size color box
-  Widget colorBox(Color color) {
-    return Container(
-      height: 100,
-      width: 100,
-      color: color,
-      margin: const EdgeInsets.all(0), // spacing between boxes
     );
   }
 }
